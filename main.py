3f5197ffb644ade2ff2ccd962cb5cf7134b46ca8
ValentinHusiaux <113856585+ValentinHusiaux@users.noreply.github.com>
2024-07-23 14:40:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, File, UploadFile, HTTPException
+import boto3
+from dotenv import load_dotenv
+from botocore.exceptions import NoCredentialsError
+from fastapi.responses import StreamingResponse
+
+load_dotenv()
 
 app = FastAPI()
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+)
 
 @app.get("/ping")
 async def ping():
     return "pong"
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    try:
+        s3.upload_fileobj(file.file, os.getenv('AWS_S3_BUCKET_NAME'), file.filename)
+        return {"filename": file.filename}
+    except NoCredentialsError:
+        raise HTTPException(status_code=400, detail="Credentials not available")
+    
+@app.get("/download/{filename}")
+async def download_file(filename: str):
+    try:
+        file_obj = s3.get_object(Bucket=os.getenv('AWS_S3_BUCKET_NAME'), Key=filename)
+        return StreamingResponse(file_obj['Body'], media_type='application/octet-stream')
+    except NoCredentialsError:
+        raise HTTPException(status_code=400, detail="Credentials not available")
+    except s3.exceptions.NoSuchKey:
+        raise HTTPException(status_code=404, detail="File not found")
