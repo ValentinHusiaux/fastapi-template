f00cfac17ad429f34391d4a6fc4f40c85e2787b4
ValentinHusiaux <113856585+ValentinHusiaux@users.noreply.github.com>
2024-07-23 12:18:28 +0200

    Update test_example.py
    Test qui vérifie si ca envoie bien pong

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,2 +1,11 @@
-def test_example():
-    assert 1 + 1 == 3
+import pytest
+from httpx import AsyncClient, ASGITransport
+from main import app
+
+@pytest.mark.asyncio
+async def test_ping():
+    transport = ASGITransport(app)
+    async with AsyncClient(transport=transport, base_url="http://test"/) as ac:
+        response = await ac.get("/ping")
+    assert response.status_code == 200
+    assert response.json() == "pong"
