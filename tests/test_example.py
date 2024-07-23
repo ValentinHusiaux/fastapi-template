db56e0e43ebf350648f283b025480cc4f157df3f
ValentinHusiaux <113856585+ValentinHusiaux@users.noreply.github.com>
2024-07-23 12:28:10 +0200

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,7 +5,7 @@ from tests.main import app
 @pytest.mark.asyncio
 async def test_ping():
     transport = ASGITransport(app)
-    async with AsyncClient(transport=transport, base_url="http://test"/) as ac:
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
         response = await ac.get("/ping")
     assert response.status_code == 200
     assert response.json() == "pong"
