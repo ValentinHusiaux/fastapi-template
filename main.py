5e12643f80428a05eee4df7d8290801cc7fc172a
ValentinHusiaux <113856585+ValentinHusiaux@users.noreply.github.com>
2024-07-24 14:59:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ s3 = boto3.client(
 )
 
 # Initialiser le client DynamoDB
-dynamodb = boto3.resource(
+dynamodb = boto3.client(
     'dynamodb',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
