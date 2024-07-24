1bc277d6147d078bfffe8e400e8823aeef29bffd
ValentinHusiaux <113856585+ValentinHusiaux@users.noreply.github.com>
2024-07-24 14:43:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -80,8 +80,7 @@ async def download_file(filename: str, request: Request):
 async def delete_file(file_id: str):
     try:
         # Retrieve the item from DynamoDB to get the filename
-        response = dynamodb.get_item(
-            TableName="FileUpload",
+        response = FileUpload.get_item(
             Key={'file_id': {'S': file_id}}
         )
 
